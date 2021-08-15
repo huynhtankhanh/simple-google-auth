da7ee59207c1e17a1cddc9fe2eecfe258920ee3a
Khanh Huynh <tankhanhhuynh@gmail.com>
2021-08-16 00:17:38 +0700

    first commit

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+
+export const Login = () => {
+  const clientId = process.env.REACT_APP_GOOGLE_API_KEY;
+
+  const [showloginButton, setShowloginButton] = useState(true);
+  const [showlogoutButton, setShowlogoutButton] = useState(false);
+  const onLoginSuccess = (res) => {
+    console.log("Login Success:", res.profileObj);
+    setShowloginButton(false);
+    setShowlogoutButton(true);
+  };
+
+  const onLoginFailure = (res) => {
+    console.log("Login Failed:", res);
+  };
+
+  const onSignoutSuccess = () => {
+    alert("You have been logged out successfully");
+    console.clear();
+    setShowloginButton(true);
+    setShowlogoutButton(false);
+  };
+
+  return (
+    <div>
+      {showloginButton ? (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign In"
+          onSuccess={onLoginSuccess}
+          onFailure={onLoginFailure}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+      ) : null}
+
+      {showlogoutButton ? (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText="Sign Out"
+          onLogoutSuccess={onSignoutSuccess}
+        ></GoogleLogout>
+      ) : null}
+    </div>
+  );
+};
